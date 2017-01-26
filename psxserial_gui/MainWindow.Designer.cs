0ca7a5ac88b8c0f806d3dccf1fd505076217be0f
Ondřej Řeháček <Ondrejrehacek@gmail.com>
2017-01-26 21:43:51 +0100

    ayyy

---
+++ psxserial_gui/MainWindow.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace psxserial_gui
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.selectexe = new System.Windows.Forms.OpenFileDialog();
+            this.probe = new System.Windows.Forms.Timer(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stat = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.console = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(950, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 28);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(814, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Playstation COM:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Browse..";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // selectexe
+            // 
+            this.selectexe.Filter = "Playstation EXE (*.exe)|*.exe|All files (*.*)|*.*";
+            this.selectexe.FileOk += new System.ComponentModel.CancelEventHandler(this.selectexe_FileOk);
+            // 
+            // probe
+            // 
+            this.probe.Enabled = true;
+            this.probe.Interval = 1000;
+            this.probe.Tick += new System.EventHandler(this.probe_Tick);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "110",
+            "300\t",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "56000",
+            "57600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(950, 95);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(148, 28);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.Text = "115200";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stat});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 569);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1134, 30);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stat
+            // 
+            this.stat.Name = "stat";
+            this.stat.Size = new System.Drawing.Size(60, 25);
+            this.stat.Text = "Ready";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(865, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Baudrate:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(891, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // console
+            // 
+            this.console.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.console.BackColor = System.Drawing.Color.Black;
+            this.console.ForeColor = System.Drawing.Color.Lime;
+            this.console.Location = new System.Drawing.Point(0, 366);
+            this.console.Multiline = true;
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(1134, 200);
+            this.console.TabIndex = 11;
+            this.console.Text = "Serial Console";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(923, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 31);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Send EXE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 338);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1134, 31);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 599);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "Push";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog selectexe;
+        private System.Windows.Forms.Timer probe;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox console;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ psxserial_gui/MainWindow.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace psxserial_gui
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            selectexe.ShowDialog();
+
+
+
+        }
+
+        private void probe_Tick(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+        }
+
+
+        public void UpdateStatus(string stats)
+        {
+            if (!InvokeRequired)
+            {
+                stat.Text = stats;
+            }
+            else
+            {
+                Invoke(new Action<string>(UpdateStatus), stats);
+            }
+        }
+
+
+        public void SetMax(int max)
+        {
+            if (!InvokeRequired)
+            {
+                progressBar1.Maximum = max;
+            }
+            else
+            {
+                Invoke(new Action<int>(SetMax), max);
+            }
+        }
+
+        public void SetCurrent(int current)
+        {
+            if (!InvokeRequired)
+            {
+                progressBar1.Value = current;
+            }
+            else
+            {
+                Invoke(new Action<int>(SetCurrent), current);
+            }
+        }
+
+        public void SendExe(string path, string com)
+        {
+            string text = path;
+            byte[] array = null;
+
+            SerialPort serialPort = new SerialPort(com, 115200, 0, 8);
+            serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "1");
+
+            serialPort.ReadTimeout = 500;
+            serialPort.WriteTimeout = 500;
+            serialPort.RtsEnable = true;
+            try
+            {
+
+                array = File.ReadAllBytes(text);
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            if (array.Length % 2048 > 0)
+            {
+                UpdateStatus("Not a valid Playstation executable !");
+            }
+            else
+            {
+                int num = array.Length / 2048;
+                Thread.Sleep(500);
+                bool flag = false;
+                while (!flag)
+                {
+                    serialPort.Write(new byte[]
+                    {
+                        99
+                    }, 0, 1);
+                    Thread.Sleep(2048);
+                    if (serialPort.BytesToRead != 0)
+                    {
+                        int num2 = serialPort.ReadByte();
+                        if (num2 != 152)
+                        {
+                            UpdateStatus("Sucess");
+                            flag = true;
+                        }
+                    }
+                }
+
+                serialPort.Write(array, 0, 2048);
+                serialPort.Write(array, 16, 4);
+                serialPort.Write(array, 24, 4);
+                serialPort.Write(array, 28, 4);
+
+                SetMax(num - 1);
+                for (int i = 1; i < num; i++)
+                {
+                    serialPort.Write(array, 2048 * i, 2048);
+                    UpdateStatus("Sending packet " + i + " of " + (num-1));
+                    SetCurrent(i);
+                }
+                UpdateStatus("Executing the PS-EXE");
+                for (int i = 0; i < 2048; i++)
+                {
+                    serialPort.Write(new byte[]
+                    {
+                        255
+                    }, 0, 1);
+                }
+                while (serialPort.BytesToWrite > 0)
+                {
+                    Thread.Sleep(100);
+                }
+                serialPort.Close();
+                UpdateStatus("Sucess");
+            }
+            SetCurrent(0);
+            SetMax(100);
+        }
+
+        private void selectexe_FileOk(object sender, CancelEventArgs e)
+        {
+            textBox1.Text = selectexe.FileName;            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string com = comboBox1.Text;
+            new Thread(() =>
+            {
+                SendExe(textBox1.Text, com);
+            }).Start();
+        }
+    }
+}
